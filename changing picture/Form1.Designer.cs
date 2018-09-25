f2b13ee6eaeedc5a77b63042e6362f9741fc4193
jacob thornton <Jet2002Jake@gmail.com>
2018-09-25 10:55:00 +0100

    picture changes when you click ont he image

--- changing picture/Form1.Designer.cs
+++ changing picture/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PictureBox2 = new System.Windows.Forms.PictureBox();
+            this.PictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox2
+            // 
+            this.PictureBox2.Image = global::changing_picture.Properties.Resources.zimbabwe;
+            this.PictureBox2.Location = new System.Drawing.Point(445, 126);
+            this.PictureBox2.Name = "PictureBox2";
+            this.PictureBox2.Size = new System.Drawing.Size(131, 89);
+            this.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox2.TabIndex = 1;
+            this.PictureBox2.TabStop = false;
+            // 
+            // PictureBox1
+            // 
+            this.PictureBox1.Image = global::changing_picture.Properties.Resources.geko;
+            this.PictureBox1.Location = new System.Drawing.Point(203, 126);
+            this.PictureBox1.Name = "PictureBox1";
+            this.PictureBox1.Size = new System.Drawing.Size(128, 89);
+            this.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.PictureBox1.TabIndex = 0;
+            this.PictureBox1.TabStop = false;
+            this.PictureBox1.Click += new System.EventHandler(this.geko_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PictureBox2);
+            this.Controls.Add(this.PictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox1;
+        private System.Windows.Forms.PictureBox PictureBox2;
     }
 }
 
--- changing picture/Form1.cs
+++ changing picture/Form1.cs
@@ -12,9 +12,26 @@ namespace changing_picture
 {
     public partial class Form1 : Form
     {
+        private Image geko;
+        private Image zimbabwe;
+
         public Form1()
         {
             InitializeComponent();
+            geko = Properties.Resources.geko;
+            zimbabwe = Properties.Resources.zimbabwe;
+        }
+
+        private void geko_Click(object sender, EventArgs e)
+        {
+            if (PictureBox1.Image == geko)
+            {
+                PictureBox1.Image = zimbabwe;
+            }
+            else
+            {
+                PictureBox1.Image = geko;
+            }
         }
     }
 }
